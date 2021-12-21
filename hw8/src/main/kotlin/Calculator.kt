9077f93dc52222e0d2f1a5394c12340d86aa9c82
dinis <dinis30094@gmail.com>
2021-12-21 17:57:18 +0300

    New

---
+++ hw8/src/main/kotlin/Calculator.kt
@@ -0,0 +1,9 @@
+package org.example
+
+class Calculator {
+
+    fun add(a: Int, b: Int) = a + b
+    fun multiply(a: Int, b: Int) = a * b
+    fun divide(a: Int, b: Int) = a / b
+    fun subtract(a: Int, b: Int) = a - b
+}
