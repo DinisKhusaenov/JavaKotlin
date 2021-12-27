b34d2eaaf8eb18a557e3101c17149c1d845c9200
dinis <dinis30094@gmail.com>
2021-12-27 19:45:27 +0300

    New

--- hw10/src/main/java/com/example/hw10/repository/CalcRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hw10.repository;
-
-import com.example.hw10.domain.Cache;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalcRepository extends JpaRepository<Cache,Integer> {
-    Cache findByFirstArgumentAndSecondArgumentAndOperator(String firstArgument, String secondArgument, String operator);
-}
