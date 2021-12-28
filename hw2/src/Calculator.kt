0edb48d88d64986b6dc4c6f1dca26ba9be76ebe2
DinisKhusaenov <dinis30094@gmail.com>
2021-12-29 01:16:50 +0300

    New

--- hw2/src/Calculator.kt
+++ hw2/src/Calculator.kt
@@ -1,11 +1,14 @@
 class Calculator {
     fun calculate(val1: Int, operation: String, val2: Int): Int {
-        return when (operation) {
-            "+" -> val1 + val2
-            "-" -> val1 - val2
-            "*" -> val1 * val2
-            "/" -> val1 / val2
-            else -> 0
-        }
+        if (operator == "/" && val2 == 0) print("Сan't divide by 0")
+        else {
+            val result = when (operator) {
+                "+" -> val1 + val2
+                "-" -> val1 - val2
+                "*" -> val1 * val2
+                "/" -> val1 / val2
+                else -> null
+            }
+	}
     }
 }
